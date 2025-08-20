734b43cba940f0cebc759fc5681cfd8abbce15e8
Degen Serenade <degenwithheart@users.noreply.github.com>
2025-08-20 11:22:07 +0100

    Added caching for all RPC/API calls

--- api/auth.ts
+++ api/auth.ts
@@ -1,6 +1,6 @@
 export const config = { runtime: 'edge' };
 
-export default async function handler(req) {
+export default async function handler(req: Request) {
   const { password } = await req.json();
   const valid = password === process.env.ACCESS_OVERRIDE_PASSWORD;
   return new Response(JSON.stringify({ valid }), {
