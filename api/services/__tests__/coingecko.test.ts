d24aa186930eb4cf32db90639e927b155c5e8f00
Degen Serenade <degenwithheart@users.noreply.github.com>
2025-09-22 22:46:40 +0100

    Finished the Theme Awareness functions & started implementing the foundations for 3D game support

--- api/services/__tests__/coingecko.test.ts
+++
@@ -1,8 +0,0 @@
-import { fetchData } from '../coingecko';
-
-test('handles error correctly', async () => {
-    const mockError = new Error('Test error');
-    const fetchDataMock = jest.fn().mockRejectedValue(mockError);
-    
-    await expect(fetchDataMock()).rejects.toThrow('Test error');
-});
